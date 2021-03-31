fddfcce8768db7025efc1a833dd9008ed1abd06b
vinaysham <81668337+vinaysham@users.noreply.github.com>
2021-03-31 11:06:51 +0530

    Update BeauticianModel.java

--- src/main/java/com/example/demo/entity/BeauticianModel.java
+++ src/main/java/com/example/demo/entity/BeauticianModel.java
@@ -1,58 +1,56 @@
-package com.example.entities;
+package com.example.demo.entity;
 
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table
-public class BeauticianModel {
-	@Id
-	@GeneratedValue
-	private String id;
-	private String name;
-	private String experience;
-	private String lawfirmName;
-	
-	
-	
-	public BeauticianModel(String id, String name, String experience, String lawfirmName) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.experience = experience;
-		this.lawfirmName = lawfirmName;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getExperience() {
-		return experience;
-	}
-	public void setExperience(String experience) {
-		this.experience = experience;
-	}
-	public String getLawfirmName() {
-		return lawfirmName;
-	}
-	public void setLawfirmName(String lawfirmName) {
-		this.lawfirmName = lawfirmName;
-	}
-	@Override
-	public String toString() {
-		return "BeauticianModel [id=" + id + ", name=" + name + ", experience=" + experience + ", lawfirmName="
-				+ lawfirmName + "]";
-	}
-	
-	
+@Table(name="BeauticianDetails")
+public class BeauticianModel{
+ 
+ @Id
+ @GeneratedValue(strategy=GenerationType.AUTO)
+ @Column(name="Beautician_id")
+ private int id;
+ 
+ @Column(name="Beautician_experience")
+ private String Expereince;
+ 
+ @Column(name="Organisation")
+ private String Organisation;
+
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public String getExpereince() {
+	return Expereince;
+}
+
+public void setExpereince(String expereince) {
+	Expereince = expereince;
+}
+
+public String getOrganisation() {
+	return Organisation;
+}
+
+public void setOrganisation(String organisation) {
+	Organisation = organisation;
+}
+
+@Override
+public String toString() {
+	return "Role [id=" + id + ", Expereince=" + Expereince + ", Organisation=" + Organisation + "]";
+}
+
+
 }
